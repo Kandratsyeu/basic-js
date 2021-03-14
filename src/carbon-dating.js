25802161bca7b9640c61f01144d2f88a37b1e242
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-03-14 19:24:44 +0300

    age of bones detected

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,20 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15
+const HALF_LIFE_PERIOD = 5730
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  const SAMPLE_ACTIVITY =
+    typeof arguments[0] === 'string'
+      ? Math.abs(parseFloat(arguments[0]))
+      : false
+
+  return SAMPLE_ACTIVITY &&
+    arguments[0] == SAMPLE_ACTIVITY &&
+    SAMPLE_ACTIVITY < MODERN_ACTIVITY
+    ? Math.ceil(
+        Math.log(MODERN_ACTIVITY / SAMPLE_ACTIVITY) /
+          (Math.log(2) / HALF_LIFE_PERIOD)
+      )
+    : false
+}
