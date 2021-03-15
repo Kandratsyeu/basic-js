94e7835d37075478b9423bb01ae10c0a70ac193d
Siarhei Kandratsyeu <sukandratsyeu@gmail.com>
2021-03-15 13:29:52 +0300

    season have been detected

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,29 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
 module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+  try {
+    if (arguments[0] && arguments[0].valueOf() == arguments[0].getTime()) {
+      let month = arguments[0].getMonth()
+      switch (true) {
+        case month < 2:
+          return 'winter'
+          break
+        case month < 5:
+          return 'spring'
+          break
+        case month < 8:
+          return 'summer'
+          break
+        case month < 11:
+          return 'fall'
+          break
+        case month == 11:
+          return 'winter'
+      }
+    } else {
+      return 'Unable to determine the time of year!'
+    }
+  } catch (e) {
+    throw new Error('Error')
+  }
+}
